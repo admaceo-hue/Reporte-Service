4f4cb491d550b6d85359152d0d1f62ffe6d3635d
Bruno0416 <brunomcalderonv@gmail.com>
2026-06-21 22:56:50 -0400

    fix: propiedades falsas para el test contextLoads()

--- src/test/java/com/mariluz/reportes/ReportesApplicationTests.java
+++ src/test/java/com/mariluz/reportes/ReportesApplicationTests.java
@@ -3,11 +3,15 @@ package com.mariluz.reportes;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+    properties = {
+        "jwt.secret=98PAzqkLUAElxtt+iNCg3GxPvqHI8ytzNfilW2S3RhQ=",
+        "eureka.client.enabled=false",
+        "eureka.client.register-with-eureka=false",
+    }
+)
 class ReportesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
